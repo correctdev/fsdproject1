f65442b2d3bc68b34e1958fc168f98d3422b00ef
Ernesto Gabriel Ponce Jr <36519578+correctdev@users.noreply.github.com>
2019-05-06 22:56:54 -0400

    Update view.sql

--- view.sql
+++ view.sql
@@ -6,10 +6,10 @@ CREATE VIEW popularauthorbyarticle AS SELECT articles.author, articles.slug, art
 
 CREATE VIEW mostpopularauthor AS SELECT popularauthorbyarticle.author, SUM (popularauthorbyarticle.hits) AS hitsbyauthor FROM popularauthorbyarticle GROUP BY popularauthorbyarticle.author;
 
-CREATE VIEW passbydays as SELECT COUNT (status) as value, DATE (time) as days FROM log WHERE status = '200 OK' GROUP BY days;
+CREATE VIEW totalbydays as SELECT COUNT (status) as value, DATE (time) as days FROM log GROUP BY days;
 
 CREATE VIEW failbydays as SELECT COUNT (status) as value, DATE (time) as days FROM log WHERE status != '200 OK' GROUP BY days;
 
-CREATE VIEW passfailbydate as SELECT passbydays.days, passbydays.value as pass, failbydays.value as fail FROM failbydays INNER JOIN passbydays ON failbydays.days = passbydays.days;
+CREATE VIEW passfailbydate as SELECT totalbydays.days, totalbydays.value as pass, failbydays.value as fail FROM failbydays INNER JOIN totalbydays ON failbydays.days = totalbydays.days;
 
 CREATE VIEW float as SELECT days, pass, fail, ((fail+0.0) / (pass+0.0))*100.0 as float FROM passfailbydate;
